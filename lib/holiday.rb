42c3c1d6ba8a9d44b47bcc26299df3c9acde139d
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-25 19:20:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts season.to_s.capitalize!
+    puts season.to_s.capitalize! + ":"
     holidays.each do |holiday, supplies|
       holiday_to_print = holiday.to_s.split("_").collect {|word| word.capitalize!}
       supplies_to_print = supplies.join(", ")
